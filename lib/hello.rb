679b3b04d484a64e2bd6c05bb4e46b2701745b22
Bryan McAfee <brymcafee@gmail.com>
2018-05-23 13:42:47 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,9 +5,12 @@ while i < array.length
   yield (array[i])
   i = i + 1
 end
-
 array
+else
+  puts "Hey! No block was given!"
 end
+end
+
 
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
